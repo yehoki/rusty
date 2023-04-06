2e9d5b0b20ac9854057ccece53afc19a14e287da
yehoki <patsenn2000@gmail.com>
2023-04-06 20:08:08 +0100

    Add more Rust print formatting

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This repo stores all basic things I will be using to learn rust syntax and any g
 ## First tutorial
 
 The first tutorial will be followed from [Rust by example](https://doc.rust-lang.org/stable/rust-by-example/index.html)
+
+We begin with the `main()` function which takes in no arguments, and is where the execution of the program begins.
+
+To print any text we can use the `println!` macro - in the Rust programming language, macros are defined by an exclamation mark `!` at the end.
--- hello.rs
+++ hello.rs
@@ -1,4 +1,33 @@
 fn main() {
     println!("Hello world!");
     println!("Test 2");
+    // The `{}` will be replaced by an argument
+    println!("{} testing", "testing");
+    // We can take positional arguments by specifying an integer inside of `{}`
+    println!("Mountain: {0},{1},{2},{1},{0}","lowest", "middle", "highest");
+    // We can do the same with named arguments:
+    println!("Object: {object}, text: {text}",
+            object = "Object text",
+            text = "text text");
+
+    // We can specify the 'width' of the text
+    println!("5 Width test: {numbertest:>5}", numbertest = 2);
+    // We can also use named arguments in the format by adding in a `$`:
+    println!("name argument format: {number:0>width$}", number = 25, width=6);
+
+    //FIXME Before:
+    // println!("My name is {0}, {1} {0}", "Bond");
+    // FIXME After:
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    
+    // #[allow(dead_code)] // disable `dead_code` which warn against unused module
+    // struct Structure(i32);
+    // fmt::Display.
+    // println!("This struct `{}` won't print...", Structure(3));
+
+
+    let pi = 3.141592;
+    println!("Pi is roughly {pi:.3}");
+
 }
